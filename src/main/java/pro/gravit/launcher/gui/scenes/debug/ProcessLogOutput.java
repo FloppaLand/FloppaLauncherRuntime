4af664ef8a0901b36141e5c69710061273c3a6f5
Zizazr <filovtv999@gmail.com>
2026-06-30 23:55:56 +0300

    fix ui upload button id

--- src/main/java/pro/gravit/launcher/gui/scenes/debug/ProcessLogOutput.java
+++ src/main/java/pro/gravit/launcher/gui/scenes/debug/ProcessLogOutput.java
@@ -9,7 +9,6 @@ import pro.gravit.launcher.gui.core.JavaFXApplication;
 import pro.gravit.launcher.gui.core.impl.ContextHelper;
 
 import java.net.URI;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
